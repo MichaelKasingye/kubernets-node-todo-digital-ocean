be405929949e40dc65e76cffa7f1c942d6422da8
michaelkasingye <michaelkasingye@gmail.com>
2023-11-27 13:13:27 +0300

    Add volumes to deployment yaml

--- src/server.js
+++ src/server.js
@@ -26,7 +26,7 @@ app.get('/', (req, res) => {
 });
 
 app.get('/health', (req, res) => {
-  res.json('This server is healthy - digital ocean');
+  res.json('This server is healthy - digital ocean - volumes');
 });
 
 app.get('/todos', (req, res) => {
